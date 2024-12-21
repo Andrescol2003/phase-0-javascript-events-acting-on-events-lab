6bef88dc10ea32a1b63b5fc3612b20fcf9002684
andres <andres.nyc.col@gmail.com>
2024-12-21 17:01:22 -0500

    Dodger

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+// Your code her
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) { // Prevent moving past the left edge
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Prevent moving past the right edge (400px - 40px)
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
